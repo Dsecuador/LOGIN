fd7d66e5d278dc90922b794ae6a4439ccb73726e
Renee <reneetoapanta@gmail.com>
2025-01-04 05:02:46 -0500

    Formulario de REGISTRO ya probado

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-import { errorMessages } from "vue/compiler-sfc";
 
 export const connectDB = async () => {
+
   try {
-    await mongoose.connect("mongodb://localhost:27017");
-    console.log(">>>>> BD Mongo Conectada ");
-  } catch (error) {
+    await mongoose.connect("mongodb://localhost/login_DB");
+    console.log(">>>>> Conectado a 'login_DB' <<<<< ");
+  } 
+  catch (error) {
     console.log(error);
   }
 };
